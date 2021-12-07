7a104c9e132a9cf336dcee98d4ddd0b7b907e142
Jean Leopoldo <jean@jettacargo.com.br>
2021-12-07 15:17:48 -0300

    fix: fixed contains function

--- main.hs
+++ main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 contains :: [Int] -> Int -> Bool
-contains elements element = element `notElem` elements
+contains elements element = element `elem` elements
 
 cellHasBeenAssigned :: [[Int]] -> Int -> Int -> Bool
 cellHasBeenAssigned grid row col =
@@ -84,37 +84,76 @@ getElementsUntilStatic line index elements =
             in
                 getElementsUntilStatic line (index-1) j
 
+-- this method goes elements by elements checking if
+-- diff between current element and next element is == 1
+-- if diff != 1, then it is not sequential
 isSequential :: [Int] -> Int -> Bool
 isSequential [] _ = True
 isSequential elements index =
-    (index == length elements +1) ||
+    index == length elements +1 ||
     (let diff = abs (elements !! index) - (elements !! index+1)
     in
         diff == 1 || isSequential elements (index+1))
 
-checkForSequentialLine :: [[Int]] -> Int -> Int -> Int -> Bool
-checkForSequentialLine grid row col number =
+-- TODO
+checkForSequentialRow :: [[Int]] -> Int -> Int -> Int -> Bool
+checkForSequentialRow grid row col number =
     let rowElements = getRowElements grid row col []
     in
         let elements = getElementsUntilStatic rowElements (length rowElements-1) []
         in
             let filtered = filter (>0) elements
             in
-                (length filtered == 1) || isSequential filtered number
+                length filtered == 1 || isSequential filtered number
+
+checkForSequentialCol:: [[Int]] -> Int -> Int -> Int -> Bool
+checkForSequentialCol grid row col number =
+    let colElements = getColElements grid row col []
+    in
+        let elements = getElementsUntilStatic colElements (length colElements-1) []
+        in
+            let filtered = filter (>0) elements
+            in
+                length filtered==1 || isSequential filtered number
+
+needsToCheckRow :: [[Int]] -> Int -> Int -> Bool
+needsToCheckRow grid row col =
+    let size = length grid
+    in
+        col == size-1 || getElement grid row (col+1) == -1
+
+needsToCheckCol :: [[Int]] -> Int -> Int -> Bool
+needsToCheckCol grid row col =
+    let size = length grid
+    in
+        row == size-1 || getElement grid (row+1) col == -1
+
+sequentialRow :: [[Int]] -> Int -> Int -> Int -> Bool
+sequentialRow grid row col number = not (needsToCheckRow grid row col) || checkForSequentialRow grid row col number
+
+sequentialCol :: [[Int]] -> Int -> Int -> Int -> Bool
+sequentialCol grid row col number = not (needsToCheckCol grid row col) || checkForSequentialCol grid row col number
 
 canAssignNumber :: [[Int]] -> Int -> Int -> Int -> Bool
 canAssignNumber grid row col number = not (cellHasBeenAssigned grid row col
                                             || hasNumberInRow grid row col number
-                                            || hasNumberInCol grid row col number
-                                            || not (checkForSequentialLine grid row col number))
+                                            || hasNumberInCol grid row col number)
+                                            && sequentialRow grid row col number
+                                            && sequentialCol grid row col number
 
+-- method recursively verifies if there is at least one number that can be placed into cell
+-- if cell is static, returns true for flow purposes
+-- three are the condition to assing a number into a cell:
+--     1. no number had been assinged to the cell
+--     2. the number must not be in the row this cell is in
+--     3. the number must not be in the column this cell is in
 canAssignAnyNumber :: [[Int]] -> Int -> Int -> Int -> Bool
 canAssignAnyNumber grid row col number =
-    number /= (length grid+1) &&
+    number /= length grid+1 &&
     (isStatic grid row col
-        || (not (cellHasBeenAssigned grid row col)
+        || not (cellHasBeenAssigned grid row col)
             && not (hasNumberInRow grid row col number)
-            && not (hasNumberInCol grid row col number))) || canAssignAnyNumber grid row col (number+1)
+            && not (hasNumberInCol grid row col number)) || canAssignAnyNumber grid row col (number+1)
 
 failedToAssignNumber :: [[Int]] -> Int -> Int -> Int -> Bool
 failedToAssignNumber grid row col number =
